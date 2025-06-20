a18d3c2a5f06859521585b7a28fada07c3fcf830
Koo <Kooroshr@hotmail.com>
2025-06-20 10:15:06 +0100

    Create 4-MedianOfTwoSortedArrays

---
+++ 4-MedianOfTwoSortedArrays/4-MedianOfTwoSortedArrays.java
@@ -0,0 +1,48 @@
+// Last updated: 20/06/2025, 10:15:06
+class Solution {
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+
+        // Always binary search the smaller array
+        if (m > n) {
+            return findMedianSortedArrays(B, A);
+        }
+
+        if (m == 0) {
+            if (n % 2 == 1) return B[n / 2];
+            return (B[n / 2 - 1] + B[n / 2]) / 2.0;
+        }
+
+        int low = 0;
+        int high = m;
+
+        while (low <= high) {
+            int i = (low + high) / 2;
+            int j = (m + n + 1) / 2 - i;
+
+            int maxLeftA  = (i == 0) ? Integer.MIN_VALUE : A[i - 1];
+            int minRightA = (i == m) ? Integer.MAX_VALUE : A[i];
+
+            int maxLeftB  = (j == 0) ? Integer.MIN_VALUE : B[j - 1];
+            int minRightB = (j == n) ? Integer.MAX_VALUE : B[j];
+
+            if (maxLeftA <= minRightB && maxLeftB <= minRightA) {
+                if ((m + n) % 2 == 0) {
+                    return (Math.max(maxLeftA, maxLeftB) + Math.min(minRightA, minRightB)) / 2.0;
+                } else {
+                    return Math.max(maxLeftA, maxLeftB);
+                }
+            } else if (maxLeftA > minRightB) {
+                high = i - 1;
+            } else {
+                low = i + 1;
+            }
+        }
+
+        throw new IllegalArgumentException("Input arrays not sorted or invalid");
+    }
+
+
+
+}
