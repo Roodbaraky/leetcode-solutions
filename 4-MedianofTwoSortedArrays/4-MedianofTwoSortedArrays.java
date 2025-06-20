8175a3a3dcba27f98beb3a012565e16fc44bb667
Koo <Kooroshr@hotmail.com>
2025-06-20 10:06:06 +0100

    File updated at 20/06/2025, 10:06:06

--- 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.java
@@ -1,4 +1,4 @@
-// Last updated: 20/06/2025, 09:57:40
+// Last updated: 20/06/2025, 10:06:06
 class Solution {
     public int romanToInt(String s) {
        //Iterate through string backwards
@@ -6,31 +6,26 @@ class Solution {
        //Look ahead to previous to check for I (VX), X (LC), C (DM)
        //-> adjust value
 
-       Map<String, Integer> charMap = new HashMap<>();
+       Map<Character, Integer> charMap = new HashMap<>();
 
-       charMap.put("I",1);
-       charMap.put("IV",4);
-       charMap.put("IX",9);
-       charMap.put("V", 5);
-       charMap.put("X", 10);
-       charMap.put("XL",40);
-       charMap.put("XC",90);
-       charMap.put("L",50);
-       charMap.put("C",100);
-       charMap.put("CD",400);
-       charMap.put("CM",900);
-       charMap.put("D", 500);
-       charMap.put("M",1000);
+       charMap.put('I',1);
+       charMap.put('V', 5);
+       charMap.put('X', 10);
+       charMap.put('L',50);
+       charMap.put('C',100);
+       charMap.put('D', 500);
+       charMap.put('M',1000);
         int total =0;
-       for (int i = s.length()-1; i>-1;i--){
-        if(i>0&&charMap.containsKey(""+s.charAt(i-1)+s.charAt(i))){
-            total+= charMap.get(""+s.charAt(i-1)+s.charAt(i));
-            i--;
+       for (int i =0 ; i<s.length();i++){
+        if(i<s.length()-1&&charMap.get(s.charAt(i))<charMap.get(s.charAt(i+1))){
+            total+=charMap.get(s.charAt(i+1))-charMap.get(s.charAt(i));
+            i++;
         }
         else{
-            total+= charMap.get(""+s.charAt(i));
+            total+=charMap.get(s.charAt(i));
         }
-       }
-       return total; 
     }
+       return total; 
+
+}
 }
